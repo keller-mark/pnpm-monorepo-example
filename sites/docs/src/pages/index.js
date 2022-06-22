cc35e619687e7c4183469601f6d1b5318121080a
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-06-22 17:34:38 -0400

    Add docs

---
+++ sites/docs/src/pages/index.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function Index() {
+  return (
+    <Layout
+      description="Example repo."
+    >
+      <h1>Some home page content</h1>
+    </Layout>
+  );
+}
