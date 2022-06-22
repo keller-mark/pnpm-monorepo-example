9a221a75e0069662e66a51ea741158ad0b629258
mkeller <7525285+keller-mark@users.noreply.github.com>
2022-06-22 17:37:59 -0400

    Try to use component in docs

--- sites/docs/src/pages/index.js
+++ sites/docs/src/pages/index.js
@@ -2,12 +2,15 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 
+import { MyComponent } from 'pnpm-ts-react-example';
+
 export default function Index() {
   return (
     <Layout
       description="Example repo."
     >
       <h1>Some home page content</h1>
+      <MyComponent color="blue" a={1} b={2} />
     </Layout>
   );
 }
