7ea7eccdef2d2892dc88a32a1354e14a4c297abd
mkeller <7525285+keller-mark@users.noreply.github.com>
2023-04-16 16:20:07 -0400

    Update

--- .changeset/post-changelog.mjs
+++ .changeset/post-changelog.mjs
@@ -5,10 +5,10 @@ import { join } from 'node:path';
 // Read the new changelog file,
 // removing the first line since it will always
 // contain the main package name.
-const newChangelog = fs.readFileSync(join('packages', 'main', 'CHANGELOG.md'), { encoding: 'utf-8' })
+const newChangelogLines = fs.readFileSync(join('packages', 'main', 'CHANGELOG.md'), { encoding: 'utf-8' })
   .split('\n')
-  .toSpliced(0, 1)
-  .join('\n');
+  .splice(0, 1);
+const newChangelog = newChangelogLines.join('\n');
 const fullChangelog = fs.readFileSync('CHANGELOG.md');
 
 fs.writeFileSync('CHANGELOG.md', newChangelog + '\n' + fullChangelog);
